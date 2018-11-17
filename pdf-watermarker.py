1b30a3c300a3430d0e3c4e5aa5d0331250bd27cb
arya <a.khaligh@aut.ac.ir>
2018-11-17 21:55:17 +0330

    It's working now.

--- pdf-watermarker.py
+++ pdf-watermarker.py
@@ -5,25 +5,24 @@ from PyPDF2 import PdfFileWriter, PdfFileReader
 c = canvas.Canvas('watermark.pdf')
 
 # Draw the image at x, y. I positioned the x,y to be where i like here
-c.drawImage('test.png', 15, 720)
+c.drawImage('test.png', 25, 10,mask='auto')
+c.drawString(15, 10, "SNEEDS.IR")
 c.save()
 
-
-# Get the watermark file you just created
 watermark = PdfFileReader(open("watermark.pdf", "rb"))
-
-# Get our files ready
 output_file = PdfFileWriter()
 input_file = PdfFileReader(open("test.pdf", "rb"))
 
-# Number of pages in input document
+
 page_count = input_file.getNumPages()
+
+
 
 # Go through all the input file pages to add a watermark to them
 for page_number in range(page_count):
     print("Watermarking page {} of {}".format(page_number, page_count))
     # merge the watermark with the page
-    input_page = input_file.getPage(page_number)
+    input_page = input_file.getPage(0)
     input_page.mergePage(watermark.getPage(0))
     # add page from input file to output document
     output_file.addPage(input_page)
