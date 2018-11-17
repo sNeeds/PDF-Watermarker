64356cf7d48ffd06a4cc0685cc776b4b820a5363
arya <a.khaligh@aut.ac.ir>
2018-11-17 22:35:45 +0330

    Canvas can draw high-quality icons.

--- pdf-watermarker.py
+++ pdf-watermarker.py
@@ -5,18 +5,15 @@ from PyPDF2 import PdfFileWriter, PdfFileReader
 c = canvas.Canvas('watermark.pdf')
 
 # Draw the image at x, y. I positioned the x,y to be where i like here
-c.drawImage('test.png', 25, 10,mask='auto')
-c.drawString(15, 10, "SNEEDS.IR")
+c.drawImage('sneeds-logo.png', 2, 2, width=30, height=30, mask='auto')
+c.drawString(37, 12, "SNEEDS.IR")
 c.save()
 
 watermark = PdfFileReader(open("watermark.pdf", "rb"))
 output_file = PdfFileWriter()
 input_file = PdfFileReader(open("test.pdf", "rb"))
 
-
 page_count = input_file.getNumPages()
-
-
 
 # Go through all the input file pages to add a watermark to them
 for page_number in range(page_count):
@@ -26,6 +23,8 @@ for page_number in range(page_count):
     input_page.mergePage(watermark.getPage(0))
     # add page from input file to output document
     output_file.addPage(input_page)
+    if page_number > 5:
+        break
 
 # finally, write "output" to document-output.pdf
 with open("document-output.pdf", "wb") as outputStream:
