418ca580db0bae8390c5911b524da73a70af1159
arya <a.khaligh@aut.ac.ir>
2018-12-09 10:13:42 +0330

    V1 release.

--- pdf-watermarker.py
+++ pdf-watermarker.py
@@ -2,7 +2,8 @@ from reportlab.pdfgen import canvas
 from PyPDF2 import PdfFileWriter, PdfFileReader
 from os import walk
 
-def get_pdf_list() :
+
+def get_pdf_list():
     f = []
     for (dirpath, dirnames, filenames) in walk("./input"):
         f.extend(filenames)
@@ -18,25 +19,24 @@ c.drawImage('sneeds-logo.png', 2, 2, width=30, height=30, mask='auto')
 c.drawString(37, 12, "SNEEDS.IR")
 c.save()
 
-
 pdf_list = get_pdf_list()
-print(pdf_list)
-watermark = PdfFileReader(open("watermark.pdf", "rb"))
-output_file = PdfFileWriter()
-input_file = PdfFileReader(open("test.pdf", "rb"))
 
-page_count = input_file.getNumPages()
+for booklet_name in pdf_list:
+    watermark = PdfFileReader(open("watermark.pdf", "rb"))
+    output_file = PdfFileWriter()
+    input_file = PdfFileReader(open("input/" + booklet_name, "rb"))
 
-# Go through all the input file pages to add a watermark to them
-for page_number in range(page_count):
-    print("Watermarking page {} of {}".format(page_number, page_count))
-    # merge the watermark with the page
-    input_page = input_file.getPage(0)
-    input_page.mergePage(watermark.getPage(0))
-    # add page from input file to output document
-    output_file.addPage(input_page)
+    page_count = input_file.getNumPages()
 
+    # Go through all the input file pages to add a watermark to them
+    for page_number in range(page_count):
+        print("Watermarking page {} of {}".format(page_number, page_count))
+        # merge the watermark with the page
+        input_page = input_file.getPage(0)
+        input_page.mergePage(watermark.getPage(0))
+        # add page from input file to output document
+        output_file.addPage(input_page)
 
-# finally, write "output" to document-output.pdf
-with open("document-output.pdf", "wb") as outputStream:
-    output_file.write(outputStream)
+    # finally, write "output" to document-output.pdf
+    with open("output/" + booklet_name, "wb") as outputStream:
+        output_file.write(outputStream)
