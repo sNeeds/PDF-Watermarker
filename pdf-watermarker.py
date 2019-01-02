1bbc57d54b0e5b1663160caf20dabf990447899e
arya <a.khaligh@aut.ac.ir>
2019-01-02 12:51:58 +0330

    Add right and left versin

--- pdf-watermarker.py
+++ pdf-watermarker.py
@@ -4,41 +4,76 @@ from PyPDF2 import PdfFileWriter, PdfFileReader
 
 
 def get_pdf_list():
-    f = []
-    for (dirpath, dirnames, filenames) in walk("./input"):
-        f.extend(filenames)
+    r = []
+    l = []
+    for (dirpath, dirnames, filenames) in walk("./left-watermarker"):
+        l.extend(filenames)
         break
-    return f
+
+    for (dirpath, dirnames, filenames) in walk("./right-watermarker"):
+        r.extend(filenames)
+        break
+    return r, l
 
 
 canvas_width = 566
 
 # Create the watermark from an image
-c = canvas.Canvas('watermark.pdf')
-
-# Draw the image at x, y. I positioned the x,y to be where i like here
-c.drawImage('sneeds-logo.png', canvas_width-4, 2, width=30, height=30, mask='auto', preserveAspectRatio=True)
-c.drawString(canvas_width-73, 12, "SNEEDS.IR")
+c = canvas.Canvas('right-watermark.pdf')
+c.drawImage('sneeds-logo.png', canvas_width - 4, 2, width=30, height=30, mask='auto', preserveAspectRatio=True)
+c.drawString(canvas_width - 73, 12, "SNEEDS.IR")
 c.save()
 
-pdf_list = get_pdf_list()
+c = canvas.Canvas('left-watermark.pdf')
+c.drawImage('sneeds-logo.png', 2, 2, width=30, height=30, mask='auto')
+c.drawString(37, 12, "SNEEDS.IR")
+c.save()
 
-for booklet_name in pdf_list:
-    watermark = PdfFileReader(open("watermark.pdf", "rb"))
+right_pdf_list, left_pdf_list = get_pdf_list()
+
+# Solving mac problem
+if right_pdf_list[0] == ".DS_Store":
+    del (right_pdf_list[0])
+
+if left_pdf_list[0] == ".DS_Store":
+    del (left_pdf_list[0])
+
+print("\nLEFT PDFs : ")
+for booklet_name in right_pdf_list:
+    watermark = PdfFileReader(open("right-watermark.pdf", "rb"))
     output_file = PdfFileWriter()
-    input_file = PdfFileReader(open("input/" + booklet_name, "rb"))
+    input_file = PdfFileReader(open("right-watermarker/" + booklet_name, "rb"))
 
     page_count = input_file.getNumPages()
 
-    # Go through all the input file pages to add a watermark to them
+    # Go through all the left-watermarker file pages to add a watermark to them
     for page_number in range(page_count):
-        print(booklet_name + ": Watermarking page {} of {}".format(page_number, page_count))
-        # merge the watermark with the page
+        print(booklet_name + ": Watermarking page {} of {}".format(page_number, page_count - 1))
+
         input_page = input_file.getPage(page_number)
         input_page.mergePage(watermark.getPage(0))
-        # add page from input file to output document
+
         output_file.addPage(input_page)
 
-    # finally, write "output" to document-output.pdf
+    with open("output/" + booklet_name, "wb") as outputStream:
+        output_file.write(outputStream)
+
+print("\nLEFT PDFs : ")
+for booklet_name in left_pdf_list:
+    watermark = PdfFileReader(open("left-watermark.pdf", "rb"))
+    output_file = PdfFileWriter()
+    input_file = PdfFileReader(open("left-watermarker/" + booklet_name, "rb"))
+
+    page_count = input_file.getNumPages()
+
+    # Go through all the left-watermarker file pages to add a watermark to them
+    for page_number in range(page_count):
+        print(booklet_name + ": Watermarking page {} of {}".format(page_number, page_count - 1))
+
+        input_page = input_file.getPage(page_number)
+        input_page.mergePage(watermark.getPage(0))
+
+        output_file.addPage(input_page)
+
     with open("output/" + booklet_name, "wb") as outputStream:
         output_file.write(outputStream)
